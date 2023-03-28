8e9de4da7f1405355662a50021c19b1ec3ca7bb4
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-03-28 22:02:05 +0530

    added bottom up solution 28th March 2023

--- 983.java
+++ 983.java
@@ -45,3 +45,51 @@ class Solution {
         
     }
 }
+
+
+// bottom up
+
+class Solution {
+    
+    public int mincostTickets(int[] days, int[] costs) {  
+         
+        Set<Integer> st = new HashSet<>();
+        
+        for(int day : days) {           
+            st.add(day);         
+        }
+        
+        int lastDay = days[days.length - 1];
+        
+        int dp[] = new int[lastDay+1];
+        
+        //dp[i] = min cost required to travel till i th day of your travel plan
+        
+        dp[0] = 0;
+        
+        for(int i=1; i<=lastDay; i++) {
+            
+            // check if you have to travel on the ith day
+            
+            if(!st.contains(i)) {
+                
+                dp[i] = dp[i-1];
+                continue;
+                
+            }
+            
+            int day1pass = costs[0] + dp[Math.max(i-1, 0)];
+            
+            int day7pass = costs[1] + dp[Math.max(i-7, 0)];
+            
+            int day30pass = costs[2] + dp[Math.max(i-30,0)];
+            
+            dp[i] = Math.min(day1pass, Math.min(day7pass, day30pass));
+            
+            
+        }
+        
+        return dp[lastDay];
+        
+    }
+}
