4decdb9906d5a73d61769f48e30ebfce88e18ef4
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-03-29 19:45:11 +0530

    added bottom up 29th March 2023

--- 1402.java
+++ 1402.java
@@ -37,3 +37,61 @@ class Solution {
         return solve(satisfaction, dp, 0, 1);
     }
 }
+
+
+// bottom up  tabulation
+
+class Solution {
+    
+    public int maxSatisfaction(int[] satisfaction) {
+        
+        int n = satisfaction.length;
+        
+        Arrays.sort(satisfaction);
+        
+        long dp[][] = new long[n+1][n+1];
+        
+        for(long[] d : dp)
+            Arrays.fill(d, Integer.MIN_VALUE);
+        
+        //dp[i][j] = max value till 0...i and time is j currently
+        
+        // at time 0, we have no values
+        
+        for(int i=0; i<n+1; i++) {
+            
+            dp[i][0] = 0;
+            
+        }
+        
+        //cooking started
+        
+        dp[0][1] = satisfaction[0];
+        
+        for(int i=1; i<n; i++) {
+            
+            for(int t=1; t<n+1; t++) {
+                
+                long include = satisfaction[i] * t + dp[i-1][t-1];
+                
+                long exclude = 0 + dp[i-1][t];
+                
+                dp[i][t] = Math.max(include, exclude);
+                
+            }
+            
+        }
+        
+        // question asks the max value could be at any time so can't return dp[n][n] blindly
+        
+        long ans = 0;
+        
+        for(int i=0; i<n+1; i++) {
+            
+            ans = Math.max(ans, dp[n-1][i]);
+            
+        }
+        
+        return (int) ans;
+    }
+}
