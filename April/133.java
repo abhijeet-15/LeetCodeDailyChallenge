f915c2ab43eff637814c39e472a62b5f45da95a3
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-08 16:46:46 +0530

    8th April 2023 added bfs solution

--- April/133.java
+++ April/133.java
@@ -46,3 +46,67 @@ class Solution {
         
     }
 }
+
+
+// bfs approach
+
+class Solution {
+    
+    HashMap<Node, Node> mp;
+    
+    private void bfs(Queue<Node> q) {
+              
+        while(!q.isEmpty()) {
+            
+            Node node = q.peek();
+            
+            Node clone_node = mp.get(node);
+            
+            q.poll();
+            
+            for(Node n : node.neighbors) {
+                
+                if(!mp.containsKey(n)) {
+                    
+                    Node clone = new Node(n.val);
+                    
+                    mp.put(n, clone);
+                    
+                    clone_node.neighbors.add(clone);
+                    
+                    q.offer(n);
+                    
+                }
+                
+                else {
+                    
+                    clone_node.neighbors.add(mp.get(n));
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    public Node cloneGraph(Node node) {
+        
+        if(node == null) return null;
+        
+        this.mp = new HashMap<>();
+        
+        Node clone_node = new Node(node.val);
+        
+        mp.put(node, clone_node);
+        
+        Queue<Node> q = new LinkedList<>();
+        
+        q.offer(node);
+        
+        bfs(q);
+        
+        return clone_node;
+        
+    }
+}
