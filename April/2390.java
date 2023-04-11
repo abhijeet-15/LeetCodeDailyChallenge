c7545d79a878bc7a20c9c52444838e66feb036d2
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-11 10:08:55 +0530

    11th April 2023

---
+++ April/2390.java
@@ -0,0 +1,93 @@
+// https://leetcode.com/problems/removing-stars-from-a-string/submissions/
+
+//approach 1 using stack
+class Solution {
+    
+    public String removeStars(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            
+            if(s.charAt(i) == '*' && !st.isEmpty())
+                st.pop();
+            
+            else 
+                st.push(s.charAt(i));
+            
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        while(!st.isEmpty()) {
+            
+            result.append(st.peek());
+            st.pop();
+            
+        }
+        
+        return result.reverse().toString();
+    }
+}
+
+
+//approach 2 using StringBuilder
+
+class Solution {
+    
+    public String removeStars(String s) {
+        
+        StringBuilder result = new StringBuilder();
+        
+        for(int i=0; i<s.length(); i++) {
+            
+            if(s.charAt(i) == '*') {
+                
+                result.deleteCharAt(result.length() - 1);
+                
+            }
+            
+            else {
+                
+                result.append(s.charAt(i));
+                
+            }
+            
+        }
+        
+        return result.toString();
+    }
+}
+
+//two pointer approach
+class Solution {
+    
+    public String removeStars(String s) {
+        
+        int n = s.length();
+        char temp [] =  new char[n];
+        
+        int j=0;
+        
+        for(int i=0; i<n; i++) {
+            
+            if(s.charAt(i) == '*')
+                j--;
+            
+            else {
+                
+                temp[j] = s.charAt(i);
+                j++;
+            }
+            
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        for(int i=0; i<j; i++) {
+            result.append(temp[i]);
+        }
+        
+        return result.toString();
+        
+    }
+}
