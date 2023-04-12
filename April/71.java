024fdc53bd5b15931910cb9b86909af83e41393e
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-12 22:30:07 +0530

    12th April 2023, added ArrayList + tokenizer solution

--- April/71.java
+++ April/71.java
@@ -55,3 +55,54 @@ class Solution {
         return result.toString();
     }
 }
+
+// using ArrayList + tokeniser --> way faster
+
+import java.util.StringTokenizer;    
+
+class Solution {
+    
+    public String simplifyPath(String path) {
+        
+        String token = "";
+        
+        StringTokenizer ss = new StringTokenizer(path, "/");
+  
+        ArrayList<String> st = new ArrayList<>();
+        
+        
+        while(ss.hasMoreTokens()) {
+            
+            token = ss.nextToken();
+            
+            if(token.equals("") || token.equals(".")) continue;
+            
+            if(!token.equals("..")) {
+                
+                st.add(token);
+                
+            }
+            else if(!st.isEmpty()) {
+                
+                st.remove(st.size()-1);
+                
+            }
+            
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        if(st.isEmpty())
+            return "/";
+        
+        for(int i=0; i<st.size(); i++) {
+            
+            result = result.append("/").append(st.get(i));
+            
+            //result = result.append("/").append(st.get(i)).append(temp);
+            
+        }
+        
+        return result.toString();
+    }
+}
