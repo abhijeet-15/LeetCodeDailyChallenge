ce3cfdb8c25b39e8fb71fa9b37157f4938104953
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-14 13:25:19 +0530

    14th April 2023 added two pointer approach

--- April/516.java
+++ April/516.java
@@ -37,3 +37,39 @@ class Solution {
         
     }
 }
+
+// Approach 2 - two pointers
+class Solution {
+    
+    private int solve(String s, int dp[][], int i, int j) {
+        
+        if(i > j) return 0;
+        
+        if(i == j) return 1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        //match
+        if(s.charAt(i) == s.charAt(j)) {
+            
+            return dp[i][j] = 2 + solve(s,dp,i+1,j-1);
+        }
+        
+        //not match
+        else {
+            
+            return dp[i][j] = Math.max(solve(s,dp,i+1,j), solve(s,dp,i,j-1));
+        }
+        
+    }
+    
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int dp[][] =  new int[n+1][n+1];
+        
+        for(int[] d : dp)
+            Arrays.fill(d,-1);
+        
+        return solve(s,dp,0,n-1);
+    }
+}
