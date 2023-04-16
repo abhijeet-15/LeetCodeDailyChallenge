d3f7c89ec025aea60a8c70cc949d3db2384c475c
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-16 18:29:51 +0530

    16th April 2023 added bottom up solution

--- April/1639.java
+++ April/1639.java
@@ -50,3 +50,53 @@ class Solution {
         return solve(freq, target, 0, 0);
     }
 }
+
+
+// approach 2
+class Solution {
+    
+    private int m;
+    private int k;
+    
+    private int dp[][];
+    private int mod;
+   
+    
+    public int numWays(String[] words, String target) {
+        
+        this.m = target.length();
+        this.k = words[0].length();
+        this.mod = 1000000007;
+        
+        this.dp = new int[1001][1001];
+        //dp[i][j] = total ways to form target of length i using dicts word of each length j
+        
+        long freq[][] = new long[26][k];
+        
+        for(int col=0; col<k; col++) {
+            for(String word : words) {
+                
+                char ch = word.charAt(col);
+                freq[ch - 'a'][col]++;
+            }
+        }
+        
+        dp[0][0] = 1;
+        
+        for(int i=0; i<=m; i++) {
+            for(int j=0; j<=k; j++) {
+                
+                //not taken
+                if(j < k)
+                    dp[i][j+1] = (dp[i][j+1] + dp[i][j]) % mod;
+                
+                //taken
+                if(i<m && j<k)
+                    dp[i+1][j+1] = (int)(((dp[i+1][j+1] + dp[i][j]) * freq[target.charAt(i)-'a'][j]) %mod);
+                
+            }
+        }
+        
+        return dp[m][k];
+    }
+}
