f7346e5c31cb07ee54186fb953339fcf7c733944
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-18 10:05:31 +0530

    18th April, 2023 - added one pointer approach

--- April/1768.java
+++ April/1768.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/merge-strings-alternately/
+
 // two pointer approach
 class Solution {
     public String mergeAlternately(String word1, String word2) {
@@ -23,6 +24,30 @@ class Solution {
         while(j < n) {
             sb.append(word2.charAt(j));
             j++;
+        }
+        
+        return sb.toString();
+    }
+}
+
+
+// one pointer
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        int i=0;
+        StringBuilder sb = new StringBuilder("");
+        
+        while(i < Math.max(m,n)) {
+            
+            if(i < m)
+                sb.append(word1.charAt(i));
+            if(i<n)
+                sb.append(word2.charAt(i));
+            
+            i++;
         }
         
         return sb.toString();
