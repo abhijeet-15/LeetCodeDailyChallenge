4e616c4d13eaf7375dd8d873eaa448237cdc6a34
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-24 11:44:28 +0530

    24th April 2023 -> using a max  Priority Queue

--- April/1046.java
+++ April/1046.java
@@ -28,3 +28,21 @@ class Solution {
         return stones[n-1];
     }
 }
+
+
+// Using Priority Queue
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        
+        for(int stone : stones) {
+            pq.offer(stone);
+        }
+        
+        while(pq.size() > 1) {
+            pq.offer(pq.poll() - pq.poll());
+        }
+        
+        return pq.poll();
+    }
+}
