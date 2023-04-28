cf45f2ef165c4cd7d1efa5035f038d4107284869
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-28 18:20:26 +0530

    28th April 2023 --> DFS solution

---
+++ April/839.java
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/similar-string-groups/
+
+class Solution {
+    
+    private boolean isSimilar(String s1, String s2) {
+        int m = s1.length();
+        int diff = 0;
+        
+        for(int i=0; i<m; i++) {
+            if(s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+            }
+        }
+        
+        return diff == 0 || diff == 2;
+    }
+    
+    private void dfs(int u, Map<Integer, List<Integer>> adj, boolean visited[]) {
+        
+        visited[u] = true;
+        
+        if(!adj.containsKey(u))
+            return;
+        
+        for(int v : adj.get(u)) {
+            if(!visited[v]) {
+                dfs(v,adj,visited);
+            }
+        }
+    }
+    
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(isSimilar(strs[i], strs[j])) {
+                    adj.computeIfAbsent(i, k->new ArrayList<Integer>()).add(j);
+                    adj.computeIfAbsent(j, k->new ArrayList<Integer>()).add(i);
+                }
+            }
+        }
+        
+        boolean visited[] = new boolean[n];
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                dfs(i,adj,visited);
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
