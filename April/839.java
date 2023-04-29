3156bf03f54c3a1c4a1bd95dee8d83e3e3921ea1
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-04-29 07:48:00 +0530

    28th April 2023 --> added DSU approach

--- April/839.java
+++ April/839.java
@@ -123,3 +123,75 @@ class Solution {
         return count;
     }
 }
+
+// DSU
+class Solution {
+    
+    private int[] parent;
+    private int[] rank;
+    
+    private int find(int x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    private void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+        
+        if(x_parent != y_parent) {
+            if(rank[x_parent] > rank[y_parent]) {
+                parent[y_parent] = x_parent;
+            }
+            else if (rank[y_parent] > rank[x_parent]) {
+                parent[x_parent] = y_parent;
+            }
+            else {
+                parent[x_parent] = y_parent;
+                rank[y_parent]++;
+            }
+        }                
+    }
+    
+    private boolean isSimilar(String s1, String s2) {
+        
+        int m = s1.length();
+        int diff = 0;
+        for(int i=0; i<m; i++) {
+            if(s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+            }
+        }
+        
+        return diff == 0 || diff == 2;
+    }
+    
+    public int numSimilarGroups(String[] strs) {
+        
+        int n = strs.length;
+        
+        this.parent = new int[n];
+        this.rank = new int[n];
+        
+        
+        for(int i=0; i<n; i++) {
+            parent[i] = i;
+        }
+        
+        int groupCount = n;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                
+                if(isSimilar(strs[i], strs[j]) && find(i) != find(j)) {
+                    Union(i,j);
+                    groupCount--;
+                }
+            }
+        }
+        
+        return groupCount;
+    }
+}
