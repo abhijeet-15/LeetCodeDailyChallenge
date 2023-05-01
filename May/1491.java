64aef0789ed8c21f835a65c709136cc7046b1e74
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-01 10:43:16 +0530

    1st May 2023 added May Folder and 1st May challenge solution

---
+++ May/1491.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+
+/*
+ time complexity O(N) where N = salary.length
+ space complexity O(1)
+ */
+
+ class Solution {
+    public double average(int[] salary) {
+        int maxi =  Integer.MIN_VALUE;
+        int mini = Integer.MAX_VALUE;
+        
+        double sum = 0;
+        
+        for(int s : salary) {
+            
+            if(s < mini) mini = s;
+            if(s > maxi) maxi = s;
+            
+            sum += s;
+        }
+    
+        return (sum-mini-maxi)/(salary.length - 2);
+    }
+}
