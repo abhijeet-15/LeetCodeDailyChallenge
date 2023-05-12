e30642d88d39c0276888a87ce3c4f624d6480198
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-12 16:41:41 +0530

    12th May 2023 added bottom up approach

--- May/Solving Questions With Brainpower.java
+++ May/Solving Questions With Brainpower.java
@@ -36,3 +36,24 @@
         return solve(questions,dp,0);
     }
 }
+
+// Bottom up
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        
+        if(n == 1){
+            return questions[0][0];
+        }
+        
+        long[] dp = new long[200001];
+        
+        //dp[i] --> max points gained by Qns from i to n-1
+        
+        for(int i=n-1; i>=0; i--) {
+            dp[i] = Math.max(questions[i][0] + dp[i + questions[i][1] + 1], dp[i+1]) ;
+        }
+        
+        return dp[0];
+    }
+}
