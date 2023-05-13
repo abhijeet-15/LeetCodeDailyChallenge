fc3d4d1eba418b5cb76b5fef4899600b22ecd1cd
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-13 13:48:49 +0530

    13 th May 2023 using Rec+Memo without loop

--- May/Count_Ways_To_Build_Good_Strings.java
+++ May/Count_Ways_To_Build_Good_Strings.java
@@ -41,3 +41,37 @@
         return count;
     }
 }
+
+// Another Memo way
+class Solution {
+    private int mod;
+    
+    private int solve(int low, int high, int zero, int one, int[] dp, int l) {
+        
+        if(l > high) {
+            return 0;
+        }
+        
+        if(dp[l] != -1) {
+            return dp[l];
+        }
+        
+        int addOne = 0;
+        
+        if(l >= low && l <=high) {
+            addOne = 1;
+        }
+        
+        int appendZero =  solve(low,high,zero,one,dp,l+zero) % mod;
+        int appendOne =  solve(low,high,zero,one,dp,l+one) % mod;
+        
+        return dp[l] =  (addOne + appendZero + appendOne) % mod;
+    }
+    
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        this.mod = 1000000007;
+        int[] dp = new int[high + 1];
+        Arrays.fill(dp, -1);
+        return solve(low, high, zero, one, dp, 0);
+    }
+}
