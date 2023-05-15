dc87e1eea813f99d3db999db795ef7dd08d9cbf1
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-05-15 09:54:29 +0530

    15th May 2023

---
+++ May/1721_Swapping_Nodes_in_a_Linked_List.java
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+
+class Solution {
+    
+    private int getLength(ListNode head) {
+        int n = 0;
+        while(head != null) {
+            n++;
+            head = head.next;
+        }
+        return n;
+    }
+    
+    public ListNode swapNodes(ListNode head, int k) {
+        
+        ListNode temp = head;
+        int n = getLength(temp);
+        
+        temp = head;
+        ListNode left = head;
+        ListNode right =  head;
+        boolean first = false;
+        boolean second = false;
+        
+        for(int i=1; i<n+1; i++) {
+            
+            if(i == k) {
+                left = temp;
+                first = true;
+            }
+            
+            if(i == n-k+1) {
+                right = temp;
+                second = true;
+            }
+            
+            if(first && second) {
+                break;
+            }
+            
+            temp = temp.next;
+        }
+        
+        int t = left.val;
+        left.val = right.val;
+        right.val = t;
+        
+        return head;
+        
+    }
+}
