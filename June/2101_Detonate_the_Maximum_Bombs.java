0794dbf01ca0977f2fbabbb74495fdc72ce9c36b
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-06-02 12:10:16 +0530

    2nd June 2023 --> added BFS solution

--- June/2101_Detonate_the_Maximum_Bombs.java
+++ June/2101_Detonate_the_Maximum_Bombs.java
@@ -48,3 +48,66 @@ class Solution {
         return answer;
     }
 }
+
+// bfs
+class Solution {
+
+    private int BFS(Map<Integer, List<Integer>> adj, int i) {
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        q.offer(i);
+        visited.add(i);
+   
+        while(!q.isEmpty()) {
+            int u = q.peek();
+            q.poll();
+            
+            //if(!adj.containsKey(u)) return visited.size();
+            
+            for(int v : adj.getOrDefault(u, new ArrayList<>())) {
+                if(!visited.contains(v)) {
+                    q.offer(v);
+                    visited.add(v);
+                }
+            }
+        }
+        return visited.size();
+    }
+    
+    public int maximumDetonation(int[][] bombs) { 
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int n = bombs.length;
+        
+        //build the graph
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                
+                if(i == j) continue;
+                long x1 = bombs[i][0];
+                long y1 = bombs[i][1];
+                long r1 = bombs[i][2];
+
+                
+                long x2 = bombs[j][0];
+                long y2 = bombs[j][1];
+                long r2 = bombs[j][2];
+                
+                long distance = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+                
+                if((long)(r1*r1) >= distance) {
+                    //edge i->j
+                    adj.computeIfAbsent(i,k->new ArrayList<>()).add(j);
+                }
+            }
+        }
+        
+        int result = 0;
+        
+        //from each node find the maximum nodes that can be visited
+        for(int i = 0; i<n; i++) {
+            int count = BFS(adj,i);
+            result = Math.max(result, count);
+        }     
+       return result;      
+    }
+}
