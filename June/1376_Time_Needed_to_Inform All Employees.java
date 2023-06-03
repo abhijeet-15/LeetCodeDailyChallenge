54a2164e4112ec4899995d2a2879edeb0a6cf5bd
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-06-03 10:32:13 +0530

    3rd June 2023 --> bfs

--- June/1376_Time_Needed_to_Inform All Employees.java
+++ June/1376_Time_Needed_to_Inform All Employees.java
@@ -33,3 +33,53 @@ class Solution {
         
     }
 }
+
+//bfs
+class Pair {
+    int first;
+    int second;
+    
+    Pair(int _first, int _second) {
+        this.first = _first;
+        this.second = _second;
+    }
+}
+
+class Solution {
+     
+    private int bfs(Map<Integer, List<Integer>> adj, int[] informTime, int i) {
+        int maxTime = Integer.MIN_VALUE;
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(i,0));
+        
+        while(!q.isEmpty()) {
+            int u = q.peek().first;
+            int currTime = q.peek().second;
+            q.poll();
+            maxTime = Math.max(maxTime, currTime);
+            
+            for(int v : adj.getOrDefault(u,new ArrayList<>())) {
+                q.offer(new Pair(v, currTime+informTime[u]));
+            }
+        }
+        
+        return maxTime;
+    }
+    
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        //create the graph O(N)
+        for(int i=0; i<manager.length; i++) {
+            int u = manager[i];
+            int v = i;
+            
+            // u --> v
+            if(manager[i] != -1)
+                adj.computeIfAbsent(u,k->new ArrayList<>()).add(v);
+        }
+        
+        return bfs(adj,informTime,headID);
+        
+    }
+}
