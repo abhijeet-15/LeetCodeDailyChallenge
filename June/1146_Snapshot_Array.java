affbe1b70e84bc4577f0f12f01b61eeebc9be08f
Abhijeet Singh <abhijeet.hcs@gmail.com>
2023-06-11 22:02:43 +0530

    11th June 2023

--- June/1146_Snapshot_Array.java
+++ June/1146_Snapshot_Array.java
@@ -24,3 +24,47 @@ class SnapshotArray {
         return historyRecords[index].floorEntry(snapId).getValue();
     }
 }
+
+// Using ArrayList
+class SnapshotArray {
+    
+    private int snapId;
+    private List<List<Pair<Integer,Integer>>> list;
+    
+    public SnapshotArray(int length) {
+        
+        this.snapId = 0;
+        this.list = new ArrayList<>(length);
+        for(int i=0; i<length; i++) {
+            List<Pair<Integer,Integer>> temp = new ArrayList<>();
+            temp.add(new Pair(0,0));
+            list.add(i,temp);
+        }
+    }
+    
+    public void set(int index, int val) {
+        list.get(index).add(new Pair(snapId, val));
+    }
+    
+    public int snap() {
+        return snapId++;
+    }
+    
+    public int get(int index, int snap_id) {
+        int start=0; int end = list.get(index).size()-1;
+        int ans = 0;
+        
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+            System.out.println(list.get(index).get(mid).getKey());
+            if((list.get(index).get(mid).getKey()) <= snap_id) {
+                ans = list.get(index).get(mid).getValue();
+                start = mid+1;
+            } 
+            else {
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+}
